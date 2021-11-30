d7bb36d7e6e2c5d852eefb39607a1a015f8cca2c
daniel692a <daniel62armas@gmail.com>
2021-11-30 16:20:33 -0600

    Add index route for API

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ from fastapi import FastAPI
 #instancia
 app = FastAPI(title="Movies", description="API para calificar películas", version="1")
 
+
+# Eventos
+@app.on_event("startup")
+def startup():
+    print('Server on')
+
+@app.on_event("shutdown")
+def shutdown():
+    print('Server off')
+
+
 @app.get("/")
-def index():
+async def index():
     return "Movies API"
+
+@app.get('/about')
+async def about():
+    return "about"
 

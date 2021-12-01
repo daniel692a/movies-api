8a6f1efe72171e4cbf47fa36f345bcff08e8bfdb
Daniel Armas Ramírez <daniel62armas@gmail.com>
2021-11-30 20:56:58 -0600

    Add username validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from database import database as connection
 from database import User, Movie, UserReview
 from schemas import UserBM
@@ -30,6 +30,10 @@ async def index():
 
 @app.post('/users')
 async def create_user(user: UserBM):
+
+    if User.select().where(User.username == user.username).exists():
+        return HTTPException(status_code=409, detail="User already exists")
+
     hash_password = User.hash_password(user.password)
     user = User.create(username=user.username, password=hash_password)
     return user.id
